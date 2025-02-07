6d2bd45b67271e889955e81c43f9c14f7b5994ff
theflyingwingman <162845902+theflyingwingman@users.noreply.github.com>
2025-02-07 15:20:05 -0800

    updated the files again as previous version had errors

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Navbar scroll effect
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        document.querySelector('.navbar').classList.add('scrolled');
+    } else {
+        document.querySelector('.navbar').classList.remove('scrolled');
+    }
+});
+
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 // Previous JavaScript remains the same
 
 // Hamburger Menu Functionality
